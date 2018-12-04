b79ae5b0916cabf47660b12f0696d96caf5529b6
alumno <alumno@aula1-03>
2018-12-04 18:19:21 +0100

    cuentas acabada

---
+++ AplicacionCuentaBancaria/src/aplicacioncuentabancaria/Menu.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplicacioncuentabancaria;
+
+/**
+ *
+ * @author alumno
+ */
+public class Menu {
+
+    /**
+     *
+     */
+    public static void mostrarMenu(){
+        System.out.println("1. Ver el número de cuenta completo.");
+        System.out.println("2. Ver el titular de la cuenta.");
+        System.out.println("3. Ver el código de la entidad.");
+        System.out.println("4. Ver el código de la oficina.");
+        System.out.println("5. Ver el número de la cuenta.");
+        System.out.println("6. Ver los dígitos de control de la cuenta.");
+        System.out.println("7. Realizar un ingreso.");
+        System.out.println("8. Retirar efectivo. ");
+        System.out.println("9. Consultar saldo.");
+        System.out.println("10. Salir de la aplicación."); 
+             
+}
+}
